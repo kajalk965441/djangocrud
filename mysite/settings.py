9d823bb6c0f79609679a04a8de463ff90554370c
unknown <kajalk965441@gmail.com>
2023-04-29 18:30:28 +0530

    allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i81xz%nqrzctk1^&bdh2)&t+wn5c)+ivu7#7et*#ayj$h-nfcf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
